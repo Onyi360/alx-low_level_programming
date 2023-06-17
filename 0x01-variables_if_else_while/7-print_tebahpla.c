64a3da244d7ab140463ee53b607b07e8999b32bf
Onyi360 <edeh717@gmail.com>
2023-06-16 18:10:46 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char lowr;
+
+	for (lowr = 'z'; lowr >= 'a'; lowr--)
+	putchar(lowr);
+	putchar('\n');
+
+	return (0);
+
+}
